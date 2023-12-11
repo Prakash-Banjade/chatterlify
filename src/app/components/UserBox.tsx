120a7d91c7e4910ae1bf3fd097b652fd1a041ad3
Prakash Banjade <prakashbanjade191@gmail.com>
2023-12-11 19:53:49 +0545

    added disabled state in UsersList

--- src/app/components/UserBox.tsx
+++ src/app/components/UserBox.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation"
 import { useCallback, useState } from "react";
 import Avatar from "./Avatar";
 import { useToast } from "@/components/ui/use-toast";
-import { Icons } from "@/components/ui/icons";
 
 type Props = {
     user: User
@@ -15,11 +14,13 @@ type Props = {
 export default function UserBox({ user }: Props) {
 
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     const { toast } = useToast();
 
     const handleClick = useCallback(
         async () => {
+            setLoading(true);
             try {
                 const res = await fetch(`/api/conversations`, {
                     method: 'POST',
@@ -48,11 +49,13 @@ export default function UserBox({ user }: Props) {
                     description: 'Something went wrong',
                     variant: 'destructive'
                 })
+            } finally {
+                setLoading(false);
             }
         }, [user, router, toast]
     )
     return (
-        <Button variant="ghost" className="w-full relative flex items-center space-x-3 px-3 justify-start py-8 gap-2" onClick={handleClick}>
+        <Button variant="ghost" className="w-full relative flex items-center space-x-3 px-3 justify-start py-8 gap-2" onClick={handleClick} disabled={loading}>
             <Avatar user={user} className="md:h-10 md:w-10 h-12 w-12" />
             <div className="flex flex-col items-start justify-start">
                 <span className="text-base font-medium">{user?.name}</span>
